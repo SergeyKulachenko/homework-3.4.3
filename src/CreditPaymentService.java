b8e5b38eaee1ceedb6b770eb901e79a0900ca578
Sergey K <89184083452.mail.ru>
2020-07-19 19:51:23 +0300

    вычисление ежемесячных выплат по банковской формуле

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,18 +1,10 @@
 public class CreditPaymentService {
     public float calculate(float sum, float persent, int period) {
-        float payment = 0, monthly_interest = 0;
-        int a, b, c = 0, period_mon;
-        float monthly_payment_main = sum / (period * 12); //сумма ежемесячного платежа на погашение основного долга
-        period_mon = period * 12; // срок кредита в месяцах
-        for (a = period_mon, b = 1; b <= a; b++) {
-            monthly_interest = sum * persent / 100 / 12; // ежемесячный % банка
-            payment = payment + (monthly_interest + monthly_payment_main);
-            sum = sum - monthly_payment_main;
-            //c++;
-            //System.out.println("процент за " + c + " месяц " + monthly_interest);
-        }
-        payment = payment / (period * 12);
+        float payment = 0, monthly_interest = 0, monthly_bid = 0, coefficient = 0;
+        monthly_bid = persent / 12 / 100;                             // месячная процентная ставка
+        coefficient = (monthly_bid * (float) Math.pow((1 + monthly_bid), period))
+                / ((float) Math.pow((1 + monthly_bid), period) - 1);  //коэффициет аннуиетета
+        payment = sum * coefficient;                                  //ежемесячный платёж
         return payment;
-
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService payment = new CreditPaymentService();
 
-        int payment1 = (int) payment.calculate(1_000_000, 9.99f, 1);
+        int payment1 = (int) payment.calculate(1_000_000, 9.99f, 12);
         System.out.println("Ежемесячный платёж при кредитовании на 1 год составляет  " + payment1 + " рублей");
 
-        int payment2 = (int)payment.calculate(1_000_000, 9.99f, 2);
+        int payment2 = (int) payment.calculate(1_000_000, 9.99f, 24);
         System.out.println("Ежемесячный платёж при кредитовании на 2 года составляет  " + payment2 + " рублей");
 
-        int payment3 = (int)payment.calculate(1_000_000, 9.99f, 3);
+        int payment3 = (int) payment.calculate(1_000_000, 9.99f, 36);
         System.out.println("Ежемесячный платёж при кредитовании на 3 года составляет  " + payment3 + " рублей");
-
 
     }
 }
